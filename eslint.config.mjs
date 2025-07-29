65ec6b01e9da711453ebb0f2496af66dd62ffdd0
Divyansh Rai <113722329+divyanshrai03@users.noreply.github.com>
2025-07-29 21:42:55 +0530

    eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Custom rule overrides
+  {
+    rules: {
+      "react/no-unescaped-entities": "off", // disables the warning about unescaped quotes in JSX
+    },
+  },
 ];
 
 export default eslintConfig;
